00f19b4541b2e8f7bc45ca69563d813f219d1125
jlyon805 <jlyon805@users.noreply.github.com>
2023-08-28 18:30:46 -0700

    Update SnakeGameManager.cs

--- Assets/Scripts/Snake/SnakeGameManager.cs
+++ Assets/Scripts/Snake/SnakeGameManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SnakeGameManager : MonoBehaviour
 {
+    private Grid<SnakeObj> grid;
+    private bool gameover = true;
+
+    [SerializeField] private int width;
+    [SerializeField] private int height;
     // Start is called before the first frame update
     void Start()
     {
-        
+        grid = new Grid<SnakeObj>(20, 20, 5f, Vector3.zero,
+             (Grid<SnakeObj> g, int x, int y) => new SnakeObj(g, x, y));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+
+public class SnakeObj
+{
+    private Grid<SnakeObj> grid;
+    int x, y;
+
+    public SnakeObj(Grid<SnakeObj> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
     }
 }
